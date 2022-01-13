9f654860d9a7f847c8a215e642dba04eb3a46913
Chuykova2005 <94622074+Chuykova2005@users.noreply.github.com>
2022-01-13 18:45:27 +0300

    Update main1.py
    Класс Dino

--- main1.py
+++ main1.py
@@ -3,30 +3,67 @@ import sys
 import pygame
 
 
+class Dino(pygame.sprite.Sprite):
+    def __init__(self, app):
+        super().__init__(app.herogroup)
+        self.image = self.load_image('dino1.png')
+        self.screen = app.screen
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+        app.screen.blit(self.image, (self.rect.x, app.height - self.rect.y))
+        self.rect = self.image.get_rect()
+        self.isjump = False
+        self.count = 25
+        self.isrun = True
+
+    def load_image(self, name, colorkey=None):
+        fullname = os.path.join('data', name)
+        print(fullname)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
+
+    def update(self, x, y):
+        self.rect.x += x
+        self.rect.y += y
+
+    def jump(self):
+        if self.count >= -25:
+            self.rect.y += self.count / 2.5
+            self.count -= 1
+        else:
+            self.isjump = False
+            self.count = 25
+
+    def run(self):
+        if self.isrun and not self.isjump:
+            self.rect.x += 5
+
+
 class App:
     def __init__(self):
         pygame.init()
         self.width, self.height = 600, 600
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption('Mario')
-        self.recty = self.height - 289
-        self.rectx = -10
-        self.fps = 50
-        self.isjump = False
-        self.count = 25
+        pygame.display.set_caption('Dino')
+        self.fps = 60
+        self.herogroup = pygame.sprite.Group()
 
     def terminate(self):
         pygame.quit()
         sys.exit()
-
-    def jump(self):
-        if self.count >= -25:
-            self.recty += self.count / 2.5
-            self.count -= 1
-        else:
-            self.isjump = False
-            self.count = 25
 
     def load_image(self, name, colorkey=None):
         fullname = os.path.join('data', name)
@@ -46,13 +83,10 @@ class App:
 
     def run_game(self):
         run = True
+        dino = Dino(self)
         while run:
-            fon = pygame.transform.scale(self.load_image('gamefon.jpg'), (self.width, self.height))
-            self.screen.blit(fon, (0, 0))
             fon2 = pygame.transform.scale(self.load_image('grass2.png'), (600, 124))
             self.screen.blit(fon2, (0, self.height - 124))
-            dino = pygame.transform.scale(self.load_image('dino1.png'), (216, 289))
-            self.screen.blit(dino, (self.rectx, self.height - self.recty))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.terminate()
@@ -61,7 +95,8 @@ class App:
                     if key[pygame.K_SPACE]:
                         self.isjump = True
                     if self.isjump:
-                        self.jump()
+                        dino.jump()
+            self.herogroup.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(self.fps)
 
