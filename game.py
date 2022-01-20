be67adc9c2a5e298239516f340c0cc7101649397
Chuykova2005 <94622074+Chuykova2005@users.noreply.github.com>
2022-01-20 10:00:06 +0300

    Update game.py

--- game.py
+++ game.py
@@ -4,7 +4,6 @@ import sys
 import pygame
 import pygame_gui
 
-
 pygame.init()
 
 
@@ -21,6 +20,14 @@ def load_image(name, folder="data", colorkey=None):
 RUN = [load_image('Run (1).png'), load_image('Run (2).png'), load_image('Run (3).png'), load_image('Run (4).png'),
        load_image('Run (5).png'), load_image('Run (6).png'), load_image('Run (7).png'), load_image('Run (8).png')]
 
+PTERO = [pygame.transform.scale(load_image("ptero1.png"), (128, 107)),
+        pygame.transform.scale(load_image("ptero2.png"), (140, 103)),
+        pygame.transform.scale(load_image("ptero3.png"), (128, 113)),
+        pygame.transform.scale(load_image("ptero4.png"), (135, 101)),
+        pygame.transform.scale(load_image("ptero5.png"), (115, 121)),
+        pygame.transform.scale(load_image("ptero6.png"), (117, 88)),
+        pygame.transform.scale(load_image("ptero7.png"), (132, 63))]
+
 
 class AnimatedDragon(pygame.sprite.Sprite):
     def __init__(self, app, sheet, columns, rows, x, y):
@@ -35,7 +42,7 @@ class AnimatedDragon(pygame.sprite.Sprite):
         self.speed = 0
 
     def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0,  0, sheet.get_width() // columns,
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
         for j in range(rows):
             for i in range(columns):
@@ -50,15 +57,17 @@ class AnimatedDragon(pygame.sprite.Sprite):
             self.image = pygame.transform.scale(self.frames[self.cur_frame], (self.rect.w, self.rect.h))
             self.speed = 0
 
+
 JUMP = [load_image('Jump (1).png'), load_image('Jump (2).png'), load_image('Jump (3).png'), load_image('Jump (4).png'),
         load_image('Jump (5).png'), load_image('Jump (6).png'), load_image('Jump (7).png'), load_image('Jump (8).png'),
-        load_image('Jump (9).png'), load_image('Jump (10).png'), load_image('Jump (11).png'), load_image('Jump (12).png')]
+        load_image('Jump (9).png'), load_image('Jump (10).png'), load_image('Jump (11).png'),
+        load_image('Jump (12).png')]
 
 for i in range(len(JUMP)):
-    JUMP[i] = pygame.transform.scale(JUMP[0], (136, 94))
+    JUMP[i] = pygame.transform.scale(JUMP[i], (136, 94))
 
 for i in range(len(RUN)):
-    RUN[i] = pygame.transform.scale(RUN[0], (136, 94))
+    RUN[i] = pygame.transform.scale(RUN[i], (136, 94))
 
 TRACE = load_image('Track.png')
 
@@ -81,6 +90,34 @@ class Cloud(pygame.sprite.Sprite):
                                        random.randrange(3) - 1)
         screen.blit(self.image, (self.rect.x, self.rect.y))
         self.count += 1
+
+
+class Ptero(pygame.sprite.Sprite):
+    """класс птеродактиля"""
+    XPOS = 700
+    YPOS = 20
+
+    def __init__(self, app, img=PTERO[0]):
+        super().__init__(app.herogroup)
+        self.image = img
+        self.isrun = True
+        self.isjump = False
+        self.rect = pygame.Rect(self.XPOS, self.YPOS, img.get_width(), img.get_width())
+        self.steps = 0
+        self.jx, self.jy = 0, 0
+
+        def fly(self):
+        self.image = PTERO[self.steps // 5]
+        # self.rect.x = self.XPOS
+        self.rect.y = [10, 10, 20, 60, 60, 60, 50][self.steps // 5] + self.YPOS
+        self.steps += 1
+        if self.steps >= 35:
+            self.steps = 0
+        if self.rect.x + self.image.get_width() < 0:
+            self.rect.x = 800
+        else:
+            self.rect.x -= 1
+        app.screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
 class Sun(pygame.sprite.Sprite):
@@ -106,6 +143,7 @@ class Sun(pygame.sprite.Sprite):
 class Dino(pygame.sprite.Sprite):
     XPOS = 80
     YPOS = 223
+    JUMP_VEL = 6.5
     JUMPSPEED = 8.5
 
     def __init__(self, app, img=RUN[0]):
@@ -116,6 +154,8 @@ class Dino(pygame.sprite.Sprite):
         self.speed = self.JUMPSPEED
         self.rect = pygame.Rect(self.XPOS, self.YPOS, img.get_width(), img.get_width())
         self.steps = 0
+        self.jump_vel = self.JUMP_VEL
+        self.jx, self.jy = 0, 0
 
     def update(self):
         if self.isrun:
@@ -129,13 +169,14 @@ class Dino(pygame.sprite.Sprite):
         self.image = RUN[self.steps // 5]
         self.rect.x = self.XPOS
         self.rect.y = self.YPOS
-        self.steps += 5
+        self.steps += 1
 
     def jump(self):
+        self.steps += 1
         self.image = JUMP[self.steps // 5]
         if self.isjump:
-            self.rect.y -= self.speed * 4
-            self.speed -= 0.8
+            self.rect.y -= self.speed * 0.6
+            self.speed -= 0.1
         if self.speed <= - self.JUMPSPEED:
             self.isjump = False
             self.isrun = True
@@ -148,7 +189,9 @@ class Dino(pygame.sprite.Sprite):
 class Obstacles(pygame.sprite.Sprite):
     images_obstracle = {'1': 'cactus1.png',
                         '2': 'cactus2.png',
-                        '3': 'cactus3.png'
+                        '3': 'cactus3.png',
+                        '4': 'rocks2.png',
+                        '5': 'rocks3.png'
                         }
 
     def __init__(self, app, type, pos):
@@ -183,10 +226,19 @@ class App:
         self.points = 0
         self.gamespeed = 20
         self.herogroup = pygame.sprite.Group()
+        self.load_sounds()
+        self.best_score = []
+        self.lives = 3
 
     def terminate(self):
         pygame.quit()
         sys.exit()
+
+    def load_sounds(self):
+        """загрузка звуков для спецэффектов"""
+        self.s_jump = pygame.mixer.Sound('data/jump.ogg') # прыжок
+        self.s_dead = pygame.mixer.Sound('data/dead.mp3')
+        self.s_fanfar = pygame.mixer.Sound('data/fanfar.mp3')  # фанфары
 
     def load_image(self, name, colorkey=None):
         fullname = os.path.join('data', name)
@@ -203,30 +255,32 @@ class App:
         else:
             image = image.convert_alpha()
         return image
-    
+
     def get_score(self):
         self.points += 1
         if self.points % 100 == 0:
             self.gamespeed += 1
-        self.points_text = f'Points: {self.points}'
+        self.points_text = f'Points: {self.points}  Lives:  {self.lives}'
         return self.points_text
 
     def generate_level(self, move):
-        fon = self.load_image('fon_level1.png')
+        fon = self.load_image('gradient.jpg')
         self.rect_fon = fon.get_rect()
         self.rect_fon.bottom = self.height
         self.rect_fon.x = move
         self.screen.fill(pygame.Color('lightblue'))
-        self.screen.blit(fon, self.rect_fon)
+        # self.screen.blit(fon, self.rect_fon)
 
     def run_game(self):
+        xxx = 0
+        pygame.mixer.music.load('data/fon_music.mp3')
+        pygame.mixer.music.play(-1)
         pygame.time.set_timer(self.myeventtype, 1000)
 
-        text = self.get_score()
         self.dinos = [Dino(self)]
-        
+
         run = True
-        #self.dino = Dino(self)
+        # self.dino = Dino(self)
         move_fon = 0
         clouds = pygame.sprite.Group()
         suns = pygame.sprite.Group()
@@ -235,9 +289,8 @@ class App:
             Cloud(800, 400).add(clouds)
             Cloud(800, 400, clouds)
         font = pygame.font.Font(None, 30)
+        self.ptero = Ptero(self)
         text_coord = 50
-        string_rendered = font.render(text, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
         while run:
             # fon2 = pygame.transform.scale(self.load_image('grass2.png'), (600, 124))
             # self.screen.blit(fon2, (0, self.height - 124))
@@ -245,43 +298,56 @@ class App:
                 if event.type == pygame.QUIT:
                     self.terminate()
                 key = pygame.key.get_pressed()
-
                 for i, dino in enumerate(self.dinos):
                     if key[pygame.K_SPACE]:
+                        self.s_jump.play()
                         dino.isjump = True
                         dino.isrun = False
 
                 if event.type == self.myeventtype:
-                    type = random.choice(['1', '2', '3'])
+                    type = random.choice(['1', '2', '3', '4', '5'])
                     print('type', type)
                     Obstacles(self, type, (self.width, 325))
                     pygame.time.set_timer(self.myeventtype, random.randrange(1000, 15000, 1000))
+            text = self.get_score()
+            string_rendered = font.render(text, 1, pygame.Color('black'))
+            intro_rect = string_rendered.get_rect()
             if move_fon > -150:
                 move_fon -= 1
             else:
                 move_fon = 0
 
-            t = pygame.sprite.spritecollide(self.dinos[0], self.all_obstacles, False, pygame.sprite.collide_rect_ratio(0.5))
+            t = pygame.sprite.spritecollide(self.dinos[0], self.all_obstacles, False,
+                                            pygame.sprite.collide_rect_ratio(0.5))
             if t:
-                print('crash')
-                self.over_game()
-                return
+                if self.lives == 0:
+                    print('crash', self.lives)
+                    self.over_game()
+                    pygame.mixer.music.stop()
+                    return
+                else:
+                    if xxx == 0:
+                        xxx = 135
+                        self.lives -= 1
+                    self.s_dead.play()
+            if xxx > 0:
+                xxx -= 1
             self.generate_level(move_fon)
             self.herogroup.draw(self.screen)
             self.all_obstacles.draw(self.screen)
             for dino in self.dinos:
                 dino.update()
                 dino.draw(self.screen)
-
             clouds.update(self.screen)
             sun.update(self.screen)
+            self.ptero.fly()
             self.screen.blit(string_rendered, intro_rect)
             self.all_obstacles.update()
-            #self.herogroup.update()
+            self.screen.blit(load_image('Track.png'), (0, 300))
+            # self.herogroup.update()
 
             pygame.display.flip()
             self.clock.tick(self.fps)
-
 
     def start_window(self):
         manager = pygame_gui.UIManager((self.width, self.height))
@@ -294,25 +360,17 @@ class App:
         drag = AnimatedDragon(self, self.load_image('animdrag.png'), 8, 2, self.width // 2 - 60, self.height // 2 - 50)
         self.anim_flag = False
         screen2 = pygame.Surface(self.screen.get_size())
-        intro_text = ["", "DRAGON RACE", "",
-                      "Правила игры",
-                      "Если в правилах несколько строк,",
-                      "приходится выводить их построчно"]
+        intro_text = [["", "DRAGON RACE", "",
+                       "Правила игры"],
+                      ["", "DRAGON RACE", "",
+                       "Игра с динозавриком, который преодолевает препятствия.",
+                       "Чтобы подпргнуть нажмите пробел. На игру дается 3 жизни",
+                       'После чего очки обнуляются и можно сыграть ещё раз.']
+                      ]
 
         fon = pygame.transform.scale(self.load_image('fon.jpg'), (self.width, self.height))
-        screen2.blit(fon, (0, 0))
-        font = pygame.font.Font(None, 30)
-        text_coord = 50
-        for line in intro_text:
-            string_rendered = font.render(line, 1, pygame.Color('black'))
-            intro_rect = string_rendered.get_rect()
-            text_coord += 10
-            intro_rect.top = text_coord
-            intro_rect.x = 90
-            text_coord += intro_rect.height
-            screen2.blit(string_rendered, intro_rect)
-
-
+        font = pygame.font.Font(None, 25)
+        sc = 0
         while True:
             time_delta = self.clock.tick(60) / 1000.0
             for event in pygame.event.get():
@@ -328,12 +386,24 @@ class App:
                 if event.type == pygame.USEREVENT:
                     if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                         if event.ui_element == btn_run:
-                            print('Вызвать основной цикл игры')
-                            return
+                            if sc == 0:
+                                sc += 1
+                            else:
+                                print('Вызвать основной цикл игры')
+                                return
                 manager.process_events(event)
             manager.update(time_delta)
             self.screen.fill(pygame.Color('#7512fa'))
-
+            screen2.blit(fon, (0, 0))
+            text_coord = 50
+            for line in intro_text[sc]:
+                string_rendered = font.render(line, 1, pygame.Color('black'))
+                intro_rect = string_rendered.get_rect()
+                text_coord += 10
+                intro_rect.top = text_coord
+                intro_rect.x = 90
+                text_coord += intro_rect.height
+                screen2.blit(string_rendered, intro_rect)
             self.screen.blit(screen2, (0, 0))
             self.anim_sprites.draw(self.screen)
             if self.anim_flag:
@@ -342,22 +412,27 @@ class App:
             pygame.display.flip()
 
     def over_game(self):
-
-        intro_text = ["Динозаврик Гугл"]
-
-        fon = pygame.transform.scale(self.load_image('fon.jpg'), (self.width, self.height))
-        self.screen.blit(fon, (0, 0))
+        self.screen.blit(load_image('gradient.jpg'), (0, 0))
+        self.best_score.append(self.points)
+        best_res = max(self.best_score)
+        intro_text = ["Вы проиграли",
+                      'Количество набранных очков:',
+                      f'{self.points}',
+                      f'Лучший результат: {best_res}',
+                      'Нажмите на экран чтобы продолжить']
+        self.points = 0
         font = pygame.font.Font(None, 30)
         text_coord = 50
         for line in intro_text:
-            string_rendered = font.render(line, 1, pygame.Color('purple'))
+            string_rendered = font.render(line, 1, pygame.Color('black'))
             intro_rect = string_rendered.get_rect()
             text_coord += 10
             intro_rect.top = text_coord
-            intro_rect.x = 10
+            intro_rect.x = 90
             text_coord += intro_rect.height
+            self.screen.blit(string_rendered, intro_rect)
 
-            # update
+        # update
 
             # render
 
@@ -370,7 +445,7 @@ class App:
                     for obstacle in self.all_obstacles:
                         obstacle.kill()
                     self.dinos[0].kill()
-
+                    self.ptero.kill()
                     self.run_game()
                     return  # начинаем игру
             pygame.display.flip()
